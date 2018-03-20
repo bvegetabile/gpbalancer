76a0aad87ca592f6277787edc904a9962ebaefa2
Brian Vegetabile <bvegetabile@gmail.com>
2018-03-20 12:49:07 -0700

    Again... never set values arbitrarily...

--- R/gpbal.R
+++ R/gpbal.R
@@ -130,7 +130,7 @@ gpbal <- function(X, y,
     message(paste('Optimal Covariate Balance:', opt_theta$fval))
   }
 
-  opt_matrix <- cov_function(as.matrix(X), c(1,opt_theta$par))
+  opt_matrix <- cov_function(as.matrix(X), opt_theta$par)
   opt_ps <- gpbal_fixed(y, opt_matrix, verbose = F, tol = 1e-2, ep_vers=ep_vers)
   opt_ps$ComputationTime <- difftime(end_time, start_time, units='secs')
   opt_ps$thetas <- opt_theta$par
