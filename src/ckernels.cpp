6c6a376d010066f6651369b1cc8b0f415969aa7e
Brian Vegetabile <bvegetabile@gmail.com>
2018-03-20 17:49:53 -0700

    changed sqexp_ard from length-scale to inc-length-scale parameterization - removes divide by zero issues

--- src/ckernels.cpp
+++ src/ckernels.cpp
@@ -42,7 +42,7 @@ arma::mat sqexp_ard(arma::mat X,
   arma::mat cov_mat(X.n_rows, X.n_rows, arma::fill::zeros);
   for(int i = 0; i < X.n_rows; i++){
     for(int j = i; j < X.n_rows; j++){
-      cov_mat(i, j) = scale * exp(- 0.5 * arma::sum(arma::pow(X.row(i) - X.row(j), 2) / arma::pow(hyperparams,2)));
+      cov_mat(i, j) = scale * exp(- 0.5 * arma::sum(arma::pow(X.row(i) - X.row(j), 2) % arma::pow(hyperparams,2)));
       cov_mat(j, i) = cov_mat(i, j);
     }
     cov_mat(i,i) = cov_mat(i,i) + noise;
