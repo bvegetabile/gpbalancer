02ef76ea514cdfc2ed77d6b67f3c55564df83ee1
Brian Vegetabile <bvegetabile@gmail.com>
2018-03-23 10:51:01 -0700

    added sqexp_poly2 kernel - 2nd order polynomials

--- R/rkernels.R
+++ R/rkernels.R
@@ -78,4 +78,18 @@ sqexp_poly <- function(X, theta, noise = 1e-4){
   return(K1 + K2)
 }
 
+sqexp_poly2 <- function(X, theta, noise = 1e-4){
+  scale0 <- 1
+  sig0 <- theta[1]
+  scale1 <- 1
+  ls1 <- theta[2]
+
+  K1 <- polykernel(X, sig0, pwr = 2,
+                   scale = scale0, noise=noise)
+  K2 <- sqexp_common(X, lengthscale = ls1,
+                     scale = scale1, noise = noise)
+
+  return(K1 + K2)
+}
+
 
