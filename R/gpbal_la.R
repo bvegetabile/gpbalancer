ad02d4d9abb2f132ce9c00dc6b7b57b8086d1061
Brian Vegetabile <bvegetabile@gmail.com>
2018-03-23 10:50:23 -0700

    adding laplace approximation capability

---
+++ R/gpbal_la.R
@@ -0,0 +1,131 @@
+gpbal_la_fixed <- function(y, cov_matrix,
+                           tol=1e-2,
+                           max_iters=20,
+                           verbose = T){
+  ##############################################################################
+  # Expectation Propagation Algorithm from Rasmussen & Williams
+  # - Algorithms: 3.5, 3.6
+  ##############################################################################
+  start_time <- Sys.time()
+  if(verbose){
+    message('Starting: Laplace Approximation for Gaussian Process Classification')
+    message('Start Time: ', start_time)
+  }
+
+  # Data checking.  Finding the classes and the number of observations
+  classes = sort(unique(y))
+  n_obs = length(y)
+
+  # Data checking to ensure correct dimensions
+  if(n_obs != nrow(cov_matrix)){
+    message(paste('Error: Vec y of length:', n_obs,
+                  'not equal to CovMat dimension:', nrow(cov_matrix), ncol(cov_matrix)))
+  }
+
+  if(length(classes) != 2){
+    message("Error: Algorithm requires two classes exactly")
+    return()
+  }
+
+  if(classes[1] == 0 && classes[2] == 1){
+    y[y==0] = -1
+    classes = sort(unique(y))
+  } else if (classes[1] != -1 || classes[2] != 1){
+    message("Error: Requires class labels -1 and 1 for Algorithm")
+    message(paste('Classes found: ', toString(classes)))
+    return()
+  }
+
+  # Converting target inputs to a column vector
+  y = matrix(y, nrow=n_obs, ncol=1)
+
+  #-----------------------------------------------------------------------------
+  # Running the Laplace Approximation Algorthim using C++ vvvvvvvvvvvvvvvvvvvv
+  #-----------------------------------------------------------------------------
+  ests <- la_probit(y, cov_matrix, tol, max_iters)
+  #-----------------------------------------------------------------------------
+  # Optimized C++ Code ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  #-----------------------------------------------------------------------------
+  end_time <- Sys.time()
+  dur_type <- 'mins'
+  dur = difftime(end_time, start_time, units=dur_type)
+  if(dur < 1){
+    dur_type <- 'secs'
+    dur = difftime(end_time, start_time, units=dur_type)
+  }
+  if(verbose){
+    message('\nEnd Time: ', end_time)
+    message('Duration: ', round(dur,3), paste(' ', dur_type, sep = ''))
+    # message('Approximate log Marginal Likelihood: ', round(results[['log_Z_ep']],3))
+    # message(paste('Number of Iterations:',results[['Number_Iters']]))
+  }
+  results <- list()
+  results[['ComputationTime']] = dur
+  results[['ps']] <- pnorm(ests)
+
+  return(results)
+}
+
+
+gpbal_la <- function(X, y,
+                     cov_function,
+                     init_theta,
+                     verbose = F,
+                     balance_metric = 'mom_sq',
+                     wts_vers='ATE'){
+
+  objective_function <- function(theta){
+    ncov <- length(theta)
+    cov_matrix <- cov_function(as.matrix(X), theta)
+    ps_res <- gpbal_la_fixed(y, cov_matrix, verbose = F, tol = 1e-2)
+    ps_est <- ps_res$ps
+
+    if(tolower(wts_vers) =='ate'){
+      ps_wts <- ifelse(y==1, 1/ps_est, 1/(1-ps_est))
+    } else if(tolower(wts_vers) == 'att') {
+      ps_wts <- ifelse(y==1, 1, ps_est/(1-ps_est))
+    } else {
+      message('invalid weighting scheme')
+      return(NULL)
+    }
+
+    if(balance_metric == 'mom_sq'){
+      cb_bal <- .mom_sq_bal(data.frame(X), 1:ncol(X), y==1, ps_wts)
+    } else if(balance_metric == 'mom'){
+      cb_bal <- .mom_bal(data.frame(X), 1:ncol(X), y==1, ps_wts)
+    } else if(balance_metric == 'ks'){
+      cb_bal <- 0
+      for(i in 1:ncol(X)){
+        cb_bal <- cb_bal + .ks_avg_test(X[,i], y, ps_est, 500)
+      }
+    } else(
+      return(NULL)
+    )
+    return(cb_bal)
+  }
+
+
+  start_time <- Sys.time()
+  if(verbose){
+    message(paste('Starting Optimization  @  ', start_time))
+  }
+  opt_theta <- minqa::bobyqa(par = init_theta,
+                             fn = objective_function,
+                             lower = rep(0, length(init_theta)))
+  end_time <- Sys.time()
+  if(verbose){
+
+    message(paste('Finished Optimization  @  ', end_time))
+    message(paste('Time Difference          :', round(difftime(end_time, start_time, units='secs'), 4)))
+
+    message(paste('Optimal Covariate Balance:', opt_theta$fval))
+  }
+
+  opt_matrix <- cov_function(as.matrix(X), opt_theta$par)
+  opt_ps <- list()
+  opt_ps$ps <- gpbal_la_fixed(y, opt_matrix, verbose = F, tol = 1e-2)$ps
+  opt_ps$ComputationTime <- difftime(end_time, start_time, units='secs')
+  opt_ps$thetas <- opt_theta$par
+  return(opt_ps)
+}
+
---
+++ src/laplaceapprox_probit.cpp
@@ -0,0 +1,56 @@
+// [[Rcpp::depends(RcppParallel)]]
+// [[Rcpp::depends(RcppArmadillo)]]
+# include <RcppArmadillo.h>
+using namespace Rcpp;
+
+// Enable C++11 via this plugin (Rcpp 0.10.3 or later)
+// [[Rcpp::plugins("cpp11")]]
+
+
+arma::vec snd(arma::vec X){
+  arma::vec Z = exp(-pow(X, 2)/2) / std::sqrt(arma::datum::pi * 2.0);
+  return(Z);
+}
+
+arma::vec snc(arma::vec X){
+  arma::vec F = 0.5 * ( 1.0 + arma::erf(X / std::sqrt(2.0)));
+  return(F);
+}
+//
+
+// [[Rcpp::export]]
+arma::vec la_probit(arma::vec targets,
+                    arma::mat covmat,
+                    double tol=1e-2,
+                    int max_iters = 20){
+  double n_obs = targets.n_elem;
+
+  arma::vec f(n_obs, arma::fill::zeros);
+  arma::vec f_old(n_obs, arma::fill::ones);
+  arma::mat W(n_obs, n_obs, arma::fill::zeros);
+  arma::mat sqrt_W(n_obs, n_obs, arma::fill::zeros);
+  arma::mat big_eye = arma::eye(n_obs, n_obs);
+  arma::mat L(n_obs, n_obs);
+  // Put loop here
+  arma::vec hess_vec(n_obs);
+  arma::vec grad_vec(n_obs);
+  arma::vec b;
+  arma::vec a;
+
+  int i = 1;
+  while(arma::mean(arma::abs(f-f_old)) >= tol){
+    f_old = f;
+    // std::cout << "Iter :" << i << "\n";
+    hess_vec = arma::pow(snd(f) / snc(targets % f), 2) + targets % f % snd(f) / snc(targets % f);
+    grad_vec = targets % snd(f) / snc(targets % f);
+    W.diag() = hess_vec;
+    sqrt_W.diag() = arma::sqrt(hess_vec);
+    L = arma::chol(big_eye + sqrt_W * covmat * sqrt_W, "lower");
+    b = W * f + grad_vec;
+    a = b - sqrt_W * arma::solve(arma::trimatu(L.t()), arma::solve(arma::trimatl(L), sqrt_W * covmat * b));
+    f = covmat * a;
+    i++;
+  }
+  // std::cout << "Mean Difference : " << arma::mean(arma::abs(f-f_old)) << "\n";
+  return(f);
+}
